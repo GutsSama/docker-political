49454fa5d109974c4b0c4a52e6dfc5e860453d12
Souhaib <massroursouhaib@hotmail.com>
2026-04-29 22:23:06 +0200

    add github action for django

--- django_political_app/django_political_app/settings.py
+++ django_political_app/django_political_app/settings.py
@@ -85,12 +85,23 @@ WSGI_APPLICATION = 'django_political_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+DB_NAME = os.environ.get("POSTGRES_DB", "postgres")
+DB_USER = os.environ.get("POSTGRES_USER", "postgres")
+DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "postgres")
+DB_HOST = os.environ.get("POSTGRES_HOST", "db") # "db" est le nom du service dans docker-compose
+DB_PORT = os.environ.get("POSTGRES_PORT", "5432")
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
+
 
 # cache pour l'affichage de la map (performance améliorée)
 CACHES = {
