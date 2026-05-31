42f395ff75b1ad7a48f30500afd2f32592190c86
John-Do59 <reynaldo@hotmail.fr>
2026-05-31 05:16:39 +0200

    refactor: remove duplicate prediction_router import and inclusion

--- api/app/routers/api.py
+++ api/app/routers/api.py
@@ -3,11 +3,9 @@ from app.endpoints.prediction_endpoints import router as prediction_router
 from app.endpoints.model_endpoints import router as model_router
 from app.endpoints.communes_endpoints import router as commune_router
 from app.endpoints.train_endpoints import router as train_router
-from app.endpoints.prediction_endpoints import router as prediction_router
 
 api_router = APIRouter()
 api_router.include_router(prediction_router)
 api_router.include_router(model_router)
 api_router.include_router(commune_router) 
 api_router.include_router(train_router)
-api_router.include_router(prediction_router)
