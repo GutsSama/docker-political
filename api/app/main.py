1d4136f6b4c3ead80bdff2bf939726b62ce141b6
Souhaib <massroursouhaib@hotmail.com>
2026-06-05 00:24:28 +0200

    fix swagger fast api

--- api/app/main.py
+++ api/app/main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 from app.routers.api import api_router
 
 app = FastAPI(
-    version="1.0.0"
+    version="1.0.0",
+    root_path="/api",
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    openapi_url="/api/openapi.json"
 )
 app.include_router(api_router)
 
