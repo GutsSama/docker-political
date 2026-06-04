9153b8116a017dcf4d9bdd767087c5bf07cf6d82
Souhaib <massroursouhaib@hotmail.com>
2026-06-04 18:54:59 +0200

    fix setting for vps

--- django_political_app/django_political_app/settings.py
+++ django_political_app/django_political_app/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-build
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'django-container']
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,0.0.0.0,django-container').split(',')
 
 # Application definition
 
